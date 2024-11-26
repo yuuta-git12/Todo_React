389ec25d81dda3a5252c5d3059ca08a750a96cd2
yuuta-git12 <tayu2201@icloud.com>
2024-11-26 17:47:48 +0900

    HTML・CSSによるモックの作成

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -4,7 +4,49 @@ import './App.css'
 export const Todo = () => {
   
   return (
-    <div>hoge</div>
+    <>
+      <div className='input-area'>
+        <input placeholder="TODOを入力?" />
+        <button>追加</button>
+      </div>
+      <div className='incomplete-area'>
+        <p className='title'>未完了のTODO</p>
+        <ul>
+          <li>
+            <div className='list-row'>
+              <p className='todo-item'>TODOです</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+          <li>
+            <div className='list-row'>
+              <p className='todo-item'>TODOです</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className='complete-area'>
+        <p className='title'>完了のTODO</p>
+          <ul>
+            <li>
+              <div className='list-row'>
+                <p className='todo-item'>TODOでした</p>
+                <button>戻す</button>
+              </div>
+            </li>
+            <li>
+              <div className='list-row'>
+                <p className='todo-item'>TODOでした</p>
+                <button>戻す</button>
+              </div>
+            </li>
+          </ul>
+      </div>
+    </>
+   
   )
 };
 
