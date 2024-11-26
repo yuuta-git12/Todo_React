780bc0fa346fb53982d4004d2bd123fed1e89f2e
yuuta-git12 <tayu2201@icloud.com>
2024-11-26 22:03:02 +0900

    リファクタリング(コンポーネント内へのスタイルの適用)

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,7 +1,17 @@
+const style = {
+    backgroundColor: "#c6e5d9",
+    width: "400px",
+    height: "45px",
+    padding: "8px",
+    margin: "8px",
+    borderRadius: "8px"  
+}
+
+
 export const InputTodo = (props) => {
     const {todoText,onChange,onClick} = props;
     return(
-        <div className='input-area'>
+        <div style={style}>
             <input 
                 placeholder="TODOを入力?" 
                 value={todoText} 
